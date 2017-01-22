02910aafeee3713e40feff833fd2d4a8f91a7ef1
Marcin Kass <marcin.kass10@gmail.com>
2017-01-22 20:38:29 +0100

    Add project files.

---
+++ Source/Year.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap
+{
+    public class Year
+    {
+        public static bool IsLeap(int year)
+        {
+            if (!DateTime.IsLeapYear(year))
+                return false;
+            return true;
+        }
+    }
+}
